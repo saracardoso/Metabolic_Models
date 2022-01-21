536379df9fb1b71cc4e4f38130c2d8f90b195e83
saracardoso <saracardoso501@gmail.com>
2022-01-21 18:23:05 +0100

    [utility data] finished adding important reactions do Tcells

--- GENERAL/code/utils/utils.R
+++ GENERAL/code/utils/utils.R
@@ -232,8 +232,14 @@ important_reactions_Tcells[['Glucose in PPP']] = c('MAR04306', 'MAR08971')
 important_reactions_Tcells[['creation of purines precursor IMP']] = 'MAR04038'
 important_reactions_Tcells[['creation of pyrimidines precursor orotate']] = 'MAR04577'
 
+# Production of nucleotides:
+important_reactions_Tcells[['nucleotide production']] = c('MAR07160')
+
 # Protein synthesis:
 important_reactions_Tcells[['protein synthesis']] = 'MAR13078'
+
+# ATP Production:
+important_reactions_Tcells[['protein synthesis']] = 'MAR06916'
 
 # Uptakes:
 important_reactions_Tcells$uptakes = list()
@@ -289,17 +295,111 @@ important_reactions_Tcells$uptakes[['SLC3A2']] = c('MAR04931', 'MAR05070', 'MAR0
                                                    'MAR08733', 'MAR08734', 'MAR09190', 'MAR09191', 'MAR00072', 'MAR06859',
                                                    'MAR06863', 'MAR06897', 'MAR07102', 'MAR07105', 'MAR07151', 'MAR07153',
                                                    'MAR07189', 'MAR11804', 'MAR11810')
-important_reactions_Tcells$uptakes[['SLC38A2']] = c()
+important_reactions_Tcells$uptakes[['SLC38A2']] = c('MAR05305', 'MAR05308', 'MAR05311', 'MAR05315', 'MAR05319', 'MAR05320',
+                                                    'MAR05322', 'MAR05324', 'MAR05326', 'MAR05333', 'MAR06375', 'MAR06380',
+                                                    'MAR06381', 'MAR06382', 'MAR06384', 'MAR11791')
+important_reactions_Tcells$uptakes[['CD36']] = c('MAR00030', 'MAR00155', 'MAR00164', 'MAR00167', 'MAR00170', 'MAR00173',
+                                                 'MAR00176', 'MAR00179', 'MAR00183', 'MAR00187', 'MAR00191', 'MAR00195',
+                                                 'MAR00199', 'MAR00203', 'MAR00208', 'MAR00212', 'MAR00216', 'MAR00225',
+                                                 'MAR00232', 'MAR00236', 'MAR00240', 'MAR00244', 'MAR00248', 'MAR00254',
+                                                 'MAR00258', 'MAR00262', 'MAR00266', 'MAR00270', 'MAR00274', 'MAR00278',
+                                                 'MAR00282', 'MAR00288', 'MAR00292', 'MAR00296', 'MAR00300', 'MAR00304',
+                                                 'MAR00308', 'MAR00312', 'MAR00318', 'MAR00322', 'MAR00326', 'MAR00330',
+                                                 'MAR00336', 'MAR00340', 'MAR00344', 'MAR00348', 'MAR00352', 'MAR00356',
+                                                 'MAR00360', 'MAR00364', 'MAR00368', 'MAR00372', 'MAR00376', 'MAR00380',
+                                                 'MAR00384', 'MAR00388', 'MAR00392', 'MAR00396', 'MAR00400', 'MAR00404',
+                                                 'MAR00408', 'MAR00412', 'MAR00416', 'MAR00420', 'MAR00424', 'MAR00428',
+                                                 'MAR00432', 'MAR00436', 'MAR11815')
+important_reactions_Tcells$uptakes[['fatty acids']] = c('MAR00240', 'MAR00428', 'MAR00380', 'MAR00288', 'MAR00254', 'MAR00432',
+                                                        'MAR00424', 'MAR00274', 'MAR00420', 'MAR00372', 'MAR00270', 'MAR00203',
+                                                        'MAR00199', 'MAR00416', 'MAR00368', 'MAR00388', 'MAR00436', 'MAR00392',
+                                                        'MAR00384', 'MAR00232', 'MAR00300', 'MAR00296', 'MAR00244', 'MAR00376',
+                                                        'MAR00364', 'MAR00360', 'MAR00412', 'MAR00408', 'MAR00312', 'MAR00340',
+                                                        'MAR00322', 'MAR00318', 'MAR00292', 'MAR00258', 'MAR00404', 'MAR00282',
+                                                        'MAR00266', 'MAR00400', 'MAR00308', 'MAR00187', 'MAR00330', 'MAR00396',
+                                                        'MAR00348', 'MAR00236', 'MAR00304', 'MAR00195', 'MAR00336', 'MAR00278',
+                                                        'MAR00262', 'MAR00356', 'MAR00216', 'MAR00225', 'MAR00212', 'MAR00208',
+                                                        'MAR00248', 'MAR00352', 'MAR00326', 'MAR00191', 'MAR00344')
+important_reactions_Tcells$uptakes[['ASCT2/SLC1A5']] = c('MAR05604', 'MAR05609', 'MAR05633', 'MAR05708', 'MAR05639', 'MAR05710')
+important_reactions_Tcells$uptakes[['proline']] = c('MAR05567', 'MAR05566', 'MAR05565', 'MAR05564', 'MAR05563', 'MAR05557',
+                                                    'MAR05551', 'MAR05544', 'MAR05536', 'MAR05527', 'MAR05517', 'MAR05506',
+                                                    'MAR05494', 'MAR05481', 'MAR05467', 'MAR05737', 'MAR05738', 'MAR05739',
+                                                    'MAR05740', 'MAR05741', 'MAR05742', 'MAR05743', 'MAR05744', 'MAR05745',
+                                                    'MAR05746', 'MAR05747', 'MAR05748', 'MAR05749', 'MAR05750', 'MAR05751',
+                                                    'MAR05877', 'MAR05878', 'MAR05879', 'MAR05880', 'MAR05881', 'MAR05322',
+                                                    'MAR06373', 'MAR07194', 'MAR08938', 'MAR09617', 'MAR08988', 'MAR08929')
+important_reactions_Tcells$uptakes[['arginine']] = c('MAR05068', 'MAR05923', 'MAR05924', 'MAR05925', 'MAR05926', 'MAR05927',
+                                                     'MAR05928', 'MAR05929', 'MAR05930', 'MAR05931', 'MAR05932', 'MAR05933',
+                                                     'MAR05934', 'MAR05935', 'MAR05936', 'MAR05937', 'MAR05938', 'MAR08629',
+                                                     'MAR09592', 'MAR09871', 'MAR11804', 'MAR11810', 'MAR05578', 'MAR05582',
+                                                     'MAR05583', 'MAR05584', 'MAR09182')
+important_reactions_Tcells$uptakes[['CD98']] = c('MAR00072', 'MAR04931', 'MAR05070', 'MAR05073', 'MAR05074', 'MAR05076',
+                                                 'MAR05077', 'MAR05078', 'MAR05079', 'MAR05080', 'MAR05082', 'MAR05084',
+                                                 'MAR05085', 'MAR05088', 'MAR05089', 'MAR05091', 'MAR05092', 'MAR05315',
+                                                 'MAR05458', 'MAR05460', 'MAR05465', 'MAR05470', 'MAR05473', 'MAR05487',
+                                                 'MAR05492', 'MAR05497', 'MAR05516', 'MAR05578', 'MAR05873', 'MAR06859',
+                                                 'MAR06863', 'MAR06897', 'MAR07102', 'MAR07105', 'MAR07151', 'MAR07153',
+                                                 'MAR07189', 'MAR07643', 'MAR07644', 'MAR08733', 'MAR08734', 'MAR09190',
+                                                 'MAR09191', 'MAR11804', 'MAR11810')
+important_reactions_Tcells$uptakes[['glutamine']] = c('MAR05308', 'MAR05310', 'MAR05617', 'MAR05618', 'MAR05619', 'MAR05620',
+                                                      'MAR05621', 'MAR05622', 'MAR05623', 'MAR05624', 'MAR05625', 'MAR05626',
+                                                      'MAR05627', 'MAR05628', 'MAR05629', 'MAR05630', 'MAR05631', 'MAR05837',
+                                                      'MAR05838', 'MAR05839', 'MAR05840', 'MAR05841', 'MAR06380', 'MAR06896',
+                                                      'MAR06897', 'MAR09887', 'MAR05078', 'MAR05459', 'MAR05473', 'MAR05487',
+                                                      'MAR05488', 'MAR05489', 'MAR05490', 'MAR05491', 'MAR05492', 'MAR05493',
+                                                      'MAR05494', 'MAR05495', 'MAR05496', 'MAR05497', 'MAR05498', 'MAR05499',
+                                                      'MAR07643', 'MAR08733')
+important_reactions_Tcells$uptakes[['acetate']] = c('MAR04934', 'MAR04935', 'MAR06001', 'MAR06008', 'MAR06015', 'MAR06022',
+                                                    'MAR06031', 'MAR06032', 'MAR06033', 'MAR06034', 'MAR06035', 'MAR06036')
+
+# Other transport reactions:
+important_reactions_Tcells$other_tranports[['pyruvate_into_mitochondria']] = c('MAR04926', 'MAR05426')
 
 # Other enzymes:
-important_reactions_Tcells[['HK2']] = c('MAR04319', 'MAR04394', 'MAR04486', 'MAR04487', 'MAR04488', 'MAR04489', 'MAR04490', 'MAR04492', 'MAR04493', 'MAR04494', 'MAR04495', 'MAR04496', 'MAR07746')
-important_reactions_Tcells[['FPK']] = c('MAR04379')
+important_reactions_Tcells[['HK2']] = c('MAR04319', 'MAR04394', 'MAR04486', 'MAR04487', 'MAR04488', 'MAR04489', 'MAR04490',
+                                        'MAR04492', 'MAR04493', 'MAR04494', 'MAR04495', 'MAR04496', 'MAR07746')
+important_reactions_Tcells[['PFK2']] = c('MAR04379')
 important_reactions_Tcells[['MCT1']] = c('MAR01517', 'MAR02151')
 important_reactions_Tcells[['LDHA']] = c('MAR01453', 'MAR04280', 'MAR04281', 'MAR04287', 'MAR04388', 'MAR11476', 'MAR11479')
 important_reactions_Tcells[['CS']] = c('MAR04145')
 important_reactions_Tcells[['OGDH']] = c('MAR04209', 'MAR04239', 'MAR04599', 'MAR05297', 'MAR06411', 'MAR06413')
 important_reactions_Tcells[['CytC']] = c('MAR06918')
-important_reactions_Tcells[['ATP5']] = c('MAR06916')
+important_reactions_Tcells[['ATP5A']] = c('MAR06916')
+important_reactions_Tcells[['ACC']] = c('MAR00030', 'MAR04156', 'MAR07672', 'MAR07673', 'MAR04295')
+important_reactions_Tcells[['GLS']] = c('MAR03892')
+important_reactions_Tcells[['GOT2']] = c('MAR01616', 'MAR03827', 'MAR03865', 'MAR03912', 'MAR04786', 'MAR06725', 'MAR06768',
+                                         'MAR06780', 'MAR08066', 'MAR08067', 'MAR08098', 'MAR08684')
+important_reactions_Tcells[['CPT1A']] = c('MAR00027', 'MAR00030', 'MAR00103', 'MAR00108', 'MAR00159', 'MAR00452', 'MAR01488',
+                                          'MAR02121', 'MAR02122', 'MAR02123', 'MAR02591', 'MAR02594', 'MAR02599', 'MAR02602',
+                                          'MAR02605', 'MAR02608', 'MAR02611', 'MAR02614', 'MAR02620', 'MAR02623', 'MAR02626',
+                                          'MAR02633', 'MAR02636', 'MAR02642', 'MAR02648', 'MAR02651', 'MAR02654', 'MAR02660',
+                                          'MAR02663', 'MAR02666', 'MAR02669', 'MAR02672', 'MAR02675', 'MAR02678', 'MAR02681',
+                                          'MAR02684', 'MAR02687', 'MAR02690', 'MAR02693', 'MAR02699', 'MAR02702', 'MAR02705',
+                                          'MAR02708', 'MAR02711', 'MAR02714', 'MAR02717', 'MAR02720', 'MAR02726', 'MAR02730',
+                                          'MAR02733', 'MAR02736', 'MAR02739', 'MAR02742', 'MAR02746', 'MAR02752', 'MAR02755',
+                                          'MAR02759', 'MAR02762', 'MAR02768', 'MAR02771', 'MAR02774', 'MAR02777', 'MAR02906',
+                                          'MAR02909', 'MAR02912', 'MAR02915', 'MAR02918', 'MAR02921', 'MAR02924', 'MAR02927',
+                                          'MAR02930', 'MAR03028', 'MAR03032', 'MAR04457', 'MAR04462', 'MAR04475', 'MAR04491',
+                                          'MAR04504', 'MAR04506', 'MAR04508', 'MAR04522', 'MAR04562', 'MAR04569', 'MAR04571',
+                                          'MAR04581', 'MAR04598', 'MAR04610', 'MAR04613', 'MAR04620', 'MAR04624', 'MAR04634',
+                                          'MAR04636', 'MAR04638', 'MAR04639', 'MAR04671', 'MAR04707', 'MAR04711', 'MAR04719',
+                                          'MAR04745', 'MAR04749', 'MAR04759', 'MAR05104', 'MAR05434', 'MAR05456', 'MAR05991',
+                                          'MAR06278', 'MAR06598', 'MAR06696')
+important_reactions_Tcells[['PCX']] = c('MAR04143')
+important_reactions_Tcells[['PDH']] = c('MAR04137')
 
+# Specific reactions:
+important_reactions_Tcells$specific[['pyruvate_to_acetylCoA']] = c('MAR04137')
+important_reactions_Tcells$specific[['PEP_to_pyruvate']] = c('MAR04358')
+important_reactions_Tcells$specific[['pyruvate_to_lactate']] = c('MAR04388')
+important_reactions_Tcells$specific[['3-PG consumption in serie biosynthesis']] = c('MAR03839')
 
+# Others:
+important_reactions_Tcells[['ornithine_production']] = c('MAR08426', 'MAR03807', 'MAR03809', 'MAR03816', 'MAR04212',
+                                                         'MAR08425', 'MAR04582')
+important_reactions_Tcells[['putrescine_production']] = c('MAR06961', 'MAR04212', 'MAR04424')
+
+# Save information in a json file:
+jsonlite::write_json(important_reactions_Tcells,
+                     './GENERAL/utility_data/important_reactions_Tcells.json')
 
